2251c33dddfe53c680df2bc7e4a85086e7aef3bc
thegibson <yukenquiries@gmail.com>
2019-04-02 15:32:30 +0100

    v12983492840921.21

--- captcha_20z/piter.js
+++ captcha_20z/piter.js
@@ -16,7 +16,7 @@
                 for (var i = 0; i < itemz.length; i++) {
                     if (itemz[i].classList.contains('phatTony')) {
                         itemz[i].classList.remove('phatTony');
-                        if (itemz[i].src.indexOf('.MOV') !== -1 || itemz[antManThanosTruck - 1].src.indexOf('.mp4') !== -1) {
+                        if (itemz[i].src.indexOf('.MOV') !== -1 || itemz[i].src.indexOf('.mp4') !== -1) {
                             console.log(1, itemz[i]);
                             itemz[i].pause();
                         }
@@ -42,7 +42,7 @@
                 for (var i = 0; i < itemz.length; i++) {
                     if (itemz[i].classList.contains('phatTony')) {
                         itemz[i].classList.remove('phatTony');
-                        if (itemz[i].src.indexOf('.MOV') !== -1 || itemz[antManThanosTruck - 1].src.indexOf('.mp4') !== -1) {
+                        if (itemz[i].src.indexOf('.MOV') !== -1 || itemz[i].src.indexOf('.mp4') !== -1) {
                             console.log(3, itemz[i]);
                             itemz[i].pause();
                         }
